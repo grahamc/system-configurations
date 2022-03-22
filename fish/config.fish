9d1df803d9b307c75307d5fbd137f16ec6c5cc97
Biggie Emmanuel <hi@bigo.lu>
2022-03-22 14:33:49 -0400

    (fish) suppress output from non-interactive fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -39,26 +39,29 @@
 # More info: https://unix.stackexchange.com/questions/38175/difference-between-login-shell-and-non-login-shell
 
 # Config for non-interactive shells e.g. shells running scripts.
-#
-# WARNING: Be sure that none of the commands in this block generate any output to stdout or stderr
-# (e.g. 'echo thisisoutput'). Since this block gets run when fish is running a script, the output
-# could break a script that expects certain content, or no content, in stdout or stderr.
-# For an example of how output could break ssh/scp/rsync, see the following link:
-# https://fishshell.com/docs/current/faq.html#why-won-t-ssh-scp-rsync-connect-properly-when-fish-is-my-login-shell
 if not status is-interactive
-    # Do not load user functions. This is because I often have functions with the same name
-    # as common commands and I don't want scripts to accidentally use them.
-    set xdg_config_home
-    if set --query XDG_CONFIG_HOME
-        set xdg_config_home $XDG_CONFIG_HOME
-    else
-        set xdg_config_home "$HOME/.config"
-    end
-    set -l user_fish_functions_directory "$xdg_config_home/fish/functions"
-    set -l index (contains --index $user_fish_functions_directory $fish_function_path)
-    if test -n "$index"
-        set --erase fish_function_path[$index]
-    end
+    # WARNING: To be safe, all code should be put inside this 'begin' block.
+    # Reason being, the code in this conditional gets run when fish is running a script so the output
+    # could break a script that expects certain content, or no content, in stdout or stderr.
+    # By using a block, I can supress any of its output to stdout or stderr.
+    #
+    # For an example of how output could break ssh/scp/rsync, see the following link:
+    # https://fishshell.com/docs/current/faq.html#why-won-t-ssh-scp-rsync-connect-properly-when-fish-is-my-login-shell
+    begin
+        # Do not load user functions. This is because I often have functions with the same name
+        # as common commands and I don't want scripts to accidentally use them.
+        set xdg_config_home
+        if set --query XDG_CONFIG_HOME
+            set xdg_config_home $XDG_CONFIG_HOME
+        else
+            set xdg_config_home "$HOME/.config"
+        end
+        set -l user_fish_functions_directory "$xdg_config_home/fish/functions"
+        set -l index (contains --index $user_fish_functions_directory $fish_function_path)
+        if test -n "$index"
+            set --erase fish_function_path[$index]
+        end
+    end >/dev/null 2>/dev/null
 end
 
 # Config for login shells i.e. shells started as part of the login process
