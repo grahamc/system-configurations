bc7eab0bf46a466f5d8c2c3ce3fef4dfa503769e
Biggie Emmanuel <hi@bigo.lu>
2022-03-24 08:22:25 -0400

    (fish) quote variable so test doesn't fail if the variable is empty

--- fish/install_fish_prompt_for_root.fish
+++ fish/install_fish_prompt_for_root.fish
@@ -2,7 +2,7 @@
 function _install_fish_prompt_for_root
     echo 'Are you sure want to install the fish prompt for the root user? (y/n): '
     read --prompt 'echo "> "' --nchars 1 response
-    if test $response != y
+    if test "$response" != y
         return
     end
     sudo install --compare --backup --suffix=.bak --owner=root --group=root --mode='u=rw,g=r,o=r' -D --verbose --no-target-directory fish/functions/fish_prompt.fish /root/.config/fish/functions/fish_prompt.fish
