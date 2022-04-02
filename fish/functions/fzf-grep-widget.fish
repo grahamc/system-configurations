7cc74679f1049e3c06e8b834e6c4f0cd20e5aa42
Biggie Emmanuel <hi@bigo.lu>
2022-04-02 11:59:17 -0400

    (fzf) fix: line wrap issue

--- fish/functions/fzf-grep-widget.fish
+++ fish/functions/fzf-grep-widget.fish
@@ -10,7 +10,8 @@ function fzf-grep-widget --description 'Search by line, recursively, from curren
             --header '(ctrl+v to open in vim)' \
             --prompt 'lines: ' \
             --preview-window '+{2}/3' \
-            --preview 'bat --terminal-width $FZF_PREVIEW_COLUMNS {1} --highlight-line {2}' \
+            # the minus 2 prevents a weird line wrap issue
+            --preview 'bat --paging=never --terminal-width (math $FZF_PREVIEW_COLUMNS - 2) {1} --highlight-line {2}' \
       )
     or return
 
