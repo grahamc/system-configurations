750fbf47319084532503664dc373b02f91a9ad7d
Biggie Emmanuel <hi@bigo.lu>
2022-04-03 12:40:07 -0400

    (cloudflared) make sure user provides a port

--- fish/functions/cloudflared-tunnel.fish
+++ fish/functions/cloudflared-tunnel.fish
@@ -1,3 +1,10 @@
 function cloudflared-tunnel --description 'Run my cloudflare tunnel with the provided url' --argument-names url
+    if test (count $argv) -eq 0
+        set function_name (status current-function)
+        echo -s \
+            (set_color red) \
+            "ERROR: You need to specify a port, e.g. '$function_name 8000'" >/dev/stderr
+        return 1
+    end
     cloudflared tunnel run --url "http://localhost:$url"
 end
