cd29cb7d68ba1b5dbac690130821f93458465297
Biggie Emmanuel <hi@bigo.lu>
2022-04-25 09:31:43 -0400

    (fish) move ctrl+c handler into a function

--- fish/config.fish
+++ fish/config.fish
@@ -194,8 +194,14 @@ if status is-interactive
     end
     bind \r _load_transient_prompt_and_execute
     # rebind ctrl+c so that before cancelling the commandline it redraws the prompt as a transient prompt
-    bind \cc 'set --global TRANSIENT; commandline -f repaint; commandline -f cancel-commandline; commandline -f repaint'
-    # rebind ctrl+d so that before exiting a shell it redraws the prompt as a transient prompt
+    function _load_transient_prompt_and_cancel
+        set --global TRANSIENT
+        commandline -f repaint
+        commandline -f cancel-commandline
+        commandline -f repaint
+    end
+    bind \cc _load_transient_prompt_and_cancel
+    # rebind ctrl+d so that before exiting the shell it redraws the prompt as a transient prompt
     function _delete_or_load_transient_prompt_and_exit
         if test -n "$(commandline)"
             commandline -f delete-char
