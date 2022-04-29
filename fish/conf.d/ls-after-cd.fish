6c9f8b397f15bf7cc838ce3583eebbf6fef253d7
Biggie Emmanuel <hi@bigo.lu>
2022-04-29 10:30:19 -0400

    (fish) wording

--- fish/conf.d/ls-after-cd.fish
+++
@@ -1,3 +0,0 @@
-function ls-after-cd --on-variable PWD
-  ls --classify --color=never --hyperlink=auto
-end
---
+++ fish/conf.d/ls-after-directory-change.fish
@@ -0,0 +1,3 @@
+function _ls-after-directory-change --on-variable PWD
+  ls --classify --color=never --hyperlink=auto
+end
