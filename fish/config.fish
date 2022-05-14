637908932b3c175b571c0c8f3238537a80dcf091
Biggie Emmanuel <hi@bigo.lu>
2022-05-14 11:12:48 -0400

    (fish) use stty instead of $COLUMNS
    Sometimes $COLUMNS doesn't seem to work, but stty always does. Plus stty is posix-compliant

--- fish/config.fish
+++ fish/config.fish
@@ -418,7 +418,7 @@ if status is-interactive
         end
 
         type --query figlet
-        and figlet -Wc -w $COLUMNS -f smblock Fish Shell v$version
+        and figlet -W -w (stty size | cut -d" " -f2) -f smblock Fish Shell v$version
         echo "Welcome back $USER, would you like to connect to tmux? (y/n):"
         read --prompt 'echo "> "' --nchars 1 response
         if test $response = y
