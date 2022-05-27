eca6c33665a4d8ee19fd159fc276f5b091b98a62
Biggie Emmanuel <hi@bigo.lu>
2022-05-26 21:47:43 -0400

    (fish) list local brew formulas and give option to search online

--- fish/functions/fzf-brew-install-widget.fish
+++ fish/functions/fzf-brew-install-widget.fish
@@ -1,12 +1,12 @@
 function fzf-brew-install-widget --description 'Install packages with brew'
   set choices \
       ( \
-        FZF_DEFAULT_COMMAND='brew search "" | tail -n +2' \
+        FZF_DEFAULT_COMMAND='brew formulae' \
          fzf-tmux -B -p 100% -- \
             --ansi \
             --multi \
-            --bind "alt-enter:toggle,change:first" \
-            --header '(alt+enter to multi-select)' \
+            --bind 'alt-enter:toggle,change:first,ctrl-o:preview(echo "Searching online...")+reload(brew search "" | tail -n +2)' \
+            --header '(alt+enter: multi-select, ctrl+o: search online)' \
             --prompt 'brew install: ' \
             --preview "HOMEBREW_COLOR=1 brew info {}" \
       )
