19c220e8b0e72284e0591bb78fb9e9613e35ff78
Biggie Emmanuel <hi@bigo.lu>
2022-05-26 22:19:35 -0400

    (fish) use fzf's loading animation while brew runs

--- fish/functions/fzf-brew-install-widget.fish
+++ fish/functions/fzf-brew-install-widget.fish
@@ -5,10 +5,19 @@ function fzf-brew-install-widget --description 'Install packages with brew'
          fzf-tmux -B -p 100% -- \
             --ansi \
             --multi \
-            --bind 'alt-enter:toggle,change:first,ctrl-o:preview(echo "Searching online...")+reload(brew search "" | tail -n +2)' \
+            # fzf triggers its loading animation for the preview window if the command hasn't completed
+            # and it has outputted at least one line. To get a loading animation for the
+            # 'brew search' command, we first echo a line of text and then enter an infinite loop.
+            # When brew is done, the preview window will get updated and the previous preview command
+            # with the infinite loop will be stopped. The inifinite loop contains a sleep command so that
+            # we don't have to execute the loop conditional as much.
+            --bind 'alt-enter:toggle,change:first,ctrl-o:preview(echo "Searching online..."; while :; sleep 1000; end)+reload(brew search "" | tail -n +2)' \
             --header '(alt+enter: multi-select, ctrl+o: search online)' \
             --prompt 'brew install: ' \
-            --preview "HOMEBREW_COLOR=1 brew info {}" \
+            # fzf triggers its loading animation for the preview window if the command hasn't completed
+            # and has outputted at least one line. To get a loading animation for the 'brew info' command
+            # we first echo a blank line and then clear it.
+            --preview 'echo ""; printf "\033[2J"; HOMEBREW_COLOR=1 brew info {}' \
       )
   or return
 
--- fish/functions/fzf-brew-uninstall-widget.fish
+++ fish/functions/fzf-brew-uninstall-widget.fish
@@ -8,7 +8,10 @@ function fzf-brew-uninstall-widget --description 'Uninstall packages with brew'
             --bind "alt-enter:toggle,change:first" \
             --header '(alt+enter to multi-select)' \
             --prompt 'brew uninstall: ' \
-            --preview "HOMEBREW_COLOR=1 brew info {}" \
+            # fzf triggers its loading animation for the preview window if the command hasn't completed
+            # and has outputted at least one line. To get a loading animation for the 'brew info' command
+            # we first echo a blank line and then clear it.
+            --preview 'echo ""; printf "\033[2J"; HOMEBREW_COLOR=1 brew info {}' \
       )
   or return
 
