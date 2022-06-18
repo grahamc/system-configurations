fa21845a5338f7a0d0ccb4b7f60251e05254682a
Biggie Emmanuel <hi@bigo.lu>
2022-06-17 20:49:14 -0400

    (pipx) prompt first before adding autocomplete

--- fish/conf.d/pipx-autocomplete.fish
+++ fish/conf.d/pipx-autocomplete.fish
@@ -4,6 +4,8 @@ end
 
 set pipx_completion_filepath "$HOME/.config/fish/completions/pipx.fish"
 if type --query pipx && not test -e $pipx_completion_filepath
-    echo 'Adding autocomplete for pipx'
-    register-python-argcomplete --shell fish pipx > $pipx_completion_filepath
+    read --prompt-str 'Autocomplete for pipx has not been setup, would you like to do that now? (y/n): ' --nchars 1 response
+    if test $response = 'y'
+        register-python-argcomplete --shell fish pipx > $pipx_completion_filepath
+    end
 end
