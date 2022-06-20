94dac7b4e59fbf8eb1f0a1b1adc2edc148efb32a
Biggie Emmanuel <hi@bigo.lu>
2022-06-20 09:20:48 -0400

    (fish) add reminder to run mandb when man fails

---
+++ fish/conf.d/mandb-reminder.fish
@@ -0,0 +1,17 @@
+# Prints a reminder to run mandb if a manpage can't be found.
+
+if not status is-interactive
+    exit
+end
+
+function mandb-reminder --on-event fish_postexec
+    set last_interactive_status $status
+    set last_interactive_pipestatus $pipestatus
+    set command (string split ' ' -- "$argv")[1]
+
+    if test "$command" = 'man'
+            and test $last_interactive_status -eq 16
+            and test (count $last_interactive_pipestatus) -eq 1
+        echo -e "\n$(set_color --bold bryellow)TIP:$(set_color normal) Try running $(set_color bryellow)'mandb'$(set_color normal) to update the manual page index cache" >/dev/stderr
+    end
+end
