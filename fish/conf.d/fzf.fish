96d919ccb7f9a4957bd3e3a45a62f7182217fcf3
Biggie Emmanuel <hi@bigo.lu>
2022-06-21 19:02:40 -0400

    (fzf) tweak margin and padding

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -9,8 +9,8 @@ set --global --export FZF_DEFAULT_OPTS "
     --layout=reverse
     --border=rounded
     --color='16,fg+:-1:regular,bg+:-1,fg:dim,info:black,gutter:bright-black,pointer:14:regular,prompt:14:regular,border:black,query:-1:regular,marker:14:regular,header:black,spinner:14,hl:cyan:dim,hl+:regular:cyan'
-    --margin=5%
-    --padding=3%
+    --margin=3%
+    --padding=1
     --height 100%
     --prompt='> '
     --tabstop=4
