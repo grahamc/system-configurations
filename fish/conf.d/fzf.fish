505205268714ee3da119c8b1e11b521a1ad29d6a
Biggie Emmanuel <hi@bigo.lu>
2022-06-23 09:13:06 -0400

    (fzf) tweak preview window toggle

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -7,7 +7,7 @@ set _fzf_history_file "$HOME/.config/fzf/fzf-history.txt"
 set --global --export FZF_DEFAULT_OPTS "
     --cycle
     --ellipsis='…'
-    --bind 'tab:down,shift-tab:up,alt-down:preview-page-down,alt-up:preview-page-up,change:first,alt-o:change-preview-window(right,border-left|bottom,border-top),ctrl-/:preview(fzf-help-preview),ctrl-\\:refresh-preview,enter:select+accept,ctrl-r:refresh-preview'
+    --bind 'tab:down,shift-tab:up,alt-down:preview-page-down,alt-up:preview-page-up,change:first,alt-o:change-preview-window(right,50%|bottom,75%),ctrl-/:preview(fzf-help-preview),ctrl-\\:refresh-preview,enter:select+accept,ctrl-r:refresh-preview'
     --layout=reverse
     --border=none
     --color='16,fg+:-1:regular,bg+:-1,fg:dim,info:black,gutter:bright-black,pointer:14:regular,prompt:14:regular,border:black:dim,query:-1:regular,marker:14:regular,header:black,spinner:yellow,hl:cyan:dim,hl+:regular:cyan'
