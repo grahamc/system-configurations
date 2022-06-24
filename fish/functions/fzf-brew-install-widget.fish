4e689ac0033816b72052551719cb064450fa4b39
Biggie Emmanuel <hi@bigo.lu>
2022-06-24 10:20:42 -0400

    don't trigger loading animation in preview window

--- fish/functions/fzf-brew-install-widget.fish
+++ fish/functions/fzf-brew-install-widget.fish
@@ -6,13 +6,7 @@ function fzf-brew-install-widget --description 'Install packages with brew'
         fzf-tmux-zoom \
             --ansi \
             --multi \
-            # fzf triggers its loading animation for the preview window if the command hasn't completed
-            # and it has outputted at least one line. To get a loading animation for the
-            # 'brew search' command, we first echo a line of text and then enter an infinite loop.
-            # When brew is done, the preview window will get updated and the previous preview command
-            # with the infinite loop will be stopped. The inifinite loop contains a sleep command so that
-            # we don't have to execute the loop conditional as much.
-            --bind 'alt-enter:toggle,change:first,ctrl-o:preview(echo "Searching online..."; while :; sleep 1000; end)+reload(brew search "" | tail -n +2)' \
+            --bind 'alt-enter:toggle,change:first,ctrl-o:preview(echo "Searching online...")+reload(brew search "" | tail -n +2)' \
             --prompt 'brew install: ' \
             # fzf triggers its loading animation for the preview window if the command hasn't completed
             # and has outputted at least one line. To get a loading animation for the 'brew info' command
