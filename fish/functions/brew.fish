456a7f6a46b80983d880a773acc0d2b2bff6b05b
Biggie Emmanuel <hi@bigo.lu>
2022-07-29 10:35:36 -0400

    (fish) add debouncing to brew package tracker

--- fish/functions/brew.fish
+++ fish/functions/brew.fish
@@ -4,7 +4,8 @@ function brew --wraps brew
 
   # By backgrounding this command in a subshell, this shell won't be its parent, the subshell will.
   # Now tmux-resurrect won't think that this backgrounded command is the command to resurrect.
-  fish -c 'flock --timeout 300 /tmp/brew-package-tracker-lock --command "chronic brew bundle dump --force --file ~/.config/brewfile/Brewfile" &'
+  # The '--nonblock' and 'sleep 10' is for debouncing.
+  fish -c 'flock --nonblock /tmp/brew-package-tracker-lock --command "sleep 10 && chronic brew bundle dump --force --file ~/.config/brewfile/Brewfile" &'
 
   return $exit_code
 end
