a98b40cccf6f7e7acefd984a9370a6d5a6e17af0
Biggie Emmanuel <hi@bigo.lu>
2022-08-30 12:31:09 -0400

    not going to copy my user shell configs to the root user anymore

--- fish/install_fish_prompt_for_root.fish
+++
@@ -1,1 +0,0 @@
-sudo install --compare --backup --suffix=.bak --owner=root --group=root --mode='u=rw,g=r,o=r' -D --verbose --no-target-directory fish/functions/fish_prompt.fish /root/.config/fish/functions/fish_prompt.fish
