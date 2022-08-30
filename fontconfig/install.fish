ffe0cb031a5e10fe9370ea76e229144e9f158bc1
Biggie Emmanuel <hi@bigo.lu>
2022-08-30 09:31:53 -0400

    if I'm running the script, I probably want to run the script

--- fontconfig/install.fish
+++ fontconfig/install.fish
@@ -1,9 +1,3 @@
-echo 'Are you sure want to install the fontconfig configs? (y/n): '
-read --prompt 'echo "> "' --nchars 1 response
-if test "$response" != y
-    return
-end
-
 sudo install --compare --backup --suffix=.bak --owner=root --group=root --mode='u=rw,g=r,o=r' -D --verbose --no-target-directory ./fontconfig/local.conf /etc/fonts/local.conf
 sudo install --compare --backup --suffix=.bak --owner=root --group=root --mode='u=rw,g=r,o=r' -D --verbose --no-target-directory ./fontconfig/10-nerd-font-symbols.conf /etc/fonts/conf.d/10-nerd-font-symbols.conf
 echo 'Reloading fontconfig configuration...'
