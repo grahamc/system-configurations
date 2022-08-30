c06af7c741f05c06ed3c4a88a34d13b21c088f4d
Biggie Emmanuel <hi@bigo.lu>
2022-08-30 09:05:54 -0400

    (fontconfig) reload cache after linking config files

--- fontconfig/install.fish
+++ fontconfig/install.fish
@@ -6,3 +6,5 @@ end
 
 sudo install --compare --backup --suffix=.bak --owner=root --group=root --mode='u=rw,g=r,o=r' -D --verbose --no-target-directory ./fontconfig/local.conf /etc/fonts/local.conf
 sudo install --compare --backup --suffix=.bak --owner=root --group=root --mode='u=rw,g=r,o=r' -D --verbose --no-target-directory ./fontconfig/10-nerd-font-symbols.conf /etc/fonts/conf.d/10-nerd-font-symbols.conf
+echo 'Reloading fontconfig configuration...'
+fc-cache -r
