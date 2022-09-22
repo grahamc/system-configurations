d4ef2464152b8c82ebd5d936db137d554cd0d40d
Biggie Emmanuel <hi@bigo.lu>
2022-09-22 16:27:49 -0400

    (fzf) scroll one line at a time with ctrl+{j,k}, sharp corners for preview window

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -13,7 +13,7 @@ end
 set --global --export FZF_DEFAULT_OPTS "
     --cycle
     --ellipsis='…'
-    --bind 'tab:down,shift-tab:up,ctrl-j:preview-page-down,ctrl-k:preview-page-up,change:first,alt-o:change-preview-window(right,60%|bottom,75%)+refresh-preview,ctrl-/:preview(fzf-help-preview)+preview-top,ctrl-\\:refresh-preview,enter:select+accept,ctrl-r:refresh-preview,ctrl-w:toggle-preview-wrap'
+    --bind 'tab:down,shift-tab:up,ctrl-j:preview-down,ctrl-k:preview-up,change:first,alt-o:change-preview-window(right,60%|bottom,75%)+refresh-preview,ctrl-/:preview(fzf-help-preview)+preview-top,ctrl-\\:refresh-preview,enter:select+accept,ctrl-r:refresh-preview,ctrl-w:toggle-preview-wrap'
     --layout=reverse
     --border=none
     --color='16,fg+:-1:regular,bg+:-1,fg:dim,info:15,gutter:8,pointer:14:regular,prompt:14:regular,border:15:dim,query:-1:regular,marker:14:regular,header:15,spinner:yellow,hl:cyan:dim,hl+:regular:cyan'
@@ -27,7 +27,7 @@ set --global --export FZF_DEFAULT_OPTS "
     --history='$_fzf_history_file'
     --header='(Press ctrl+/ for help)'
     --preview='echo Current selection: {}'
-    --preview-window=wrap,bottom,border-rounded,75%"
+    --preview-window=wrap,bottom,border-sharp,75%"
 
 set --global --export FZF_ALT_C_COMMAND 'test $dir = '.' && set _args "--strip-cwd-prefix" || set _args '.' $dir; fd $_args --hidden --type directory --type symlink'
 set --global --export FZF_ALT_C_OPTS "--preview 'ls --classify -x {}' --keep-right --bind='change:first'"
