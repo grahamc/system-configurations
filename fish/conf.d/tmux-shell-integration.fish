1cd58c7d083030fd56f3743ca0208cc8f7659622
Biggie Emmanuel <hi@bigo.lu>
2022-09-29 17:08:13 -0400

    empty checks

--- fish/conf.d/tmux-shell-integration.fish
+++ fish/conf.d/tmux-shell-integration.fish
@@ -30,11 +30,18 @@ end
 bind \eo __view_last_command_output_in_fzf
 
 function __get_last_pane_line
+    set cursor_line (tmux display -p '#{cursor_y}')
+    if test -z "$cursor_line"
+        return
+    end
     # the number returned from tmux is 0-based so add 1
     # since fish arrays are 1-based
-    set cursor_line (math (tmux display -p '#{cursor_y}') + 1)
-    set pane_line_count (count (tmux capture-pane -p -J -S - -E - | string split \n))
+    set cursor_line (math $cursor_line + 1)
 
+    set pane_line_count (count (tmux capture-pane -p -J -S - -E - | string split \n))
+    if test -z "$pane_line_count"
+        return
+    end
     # When getting the lines in the pane tmux will include any blank lines at the bottom that haven't been printed onto yet.
     # This removes those lines from the count.
     set blank_line_count (math $LINES - $cursor_line)
@@ -47,11 +54,21 @@ function __mark_output_begin --on-event fish_preexec
 end
 
 function __mark_output_end --on-event fish_postexec
-    set output_end (math (__get_last_pane_line) - 1)
-    tmux set-option -p '@output_end' $output_end
+    set last_line (__get_last_pane_line)
+    if test -z "$last_line"
+        return
+    end
+    set output_end (math $last_line - 1)
+    if test -z "$output_end"
+        return
+    end
+    tmux set-option -p '@output_end' "$output_end"
 
     # if output is larger than screen, show tip
     set output_begin (tmux display -p '#{@output_begin}')
+    if test -z "$output_begin"
+        return
+    end
     set output_line_count (math \( $output_end - $output_begin \) + 1)
     if test $output_line_count -gt $LINES
         echo -e "\n[$(set_color yellow)tip$(set_color normal)] View the output of the last command in $(set_color blue)less$(set_color normal) with $(set_color blue)ctrl+o$(set_color normal) or $(set_color magenta)fzf$(set_color normal) with $(set_color magenta)alt+o$(set_color normal)"
