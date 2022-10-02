01b1f40c496cc16117656c5966668249731885b8
Biggie Emmanuel <hi@bigo.lu>
2022-10-02 09:21:30 -0400

    fix: poll to confirm tmux server is down after calling kill-server

--- fish/functions/reload-tmux-server.fish
+++ fish/functions/reload-tmux-server.fish
@@ -1,22 +1,40 @@
+function is_tmux_running
+    tmux list-sessions &>/dev/null
+end
+
 function reload-tmux-server --description 'Reload tmux server'
     # Not sure how to restart the tmux server and reconnect to it
-    # from within a tmux session
+    # from a tmux-managed shell.
     if test -n "$TMUX"
         echo -s \
             (set_color red) \
-            'ERROR: Unable to reload the tmux server from within a tmux session, please dettach from the session and try again.' \
+            'ERROR: Unable to reload the tmux server from a tmux-managed shell, please detach from the server and try again.' \
             (set_color normal)
         return 1
     end
 
-    if tmux list-sessions &>/dev/null # tmux is running
+    if is_tmux_running
         # detach all clients from all sessions. We suppress stderr because tmux prints out
         # error text if we try to detach all clients on a session that has no clients
         tmux list-sessions -F '#{session_name}' | xargs -I SESSION tmux detach-client -s SESSION 2>/dev/null
         # save server state
         ~/.tmux/plugins/tmux-resurrect/scripts/save.sh
 
+        # The tmux server is not actually shut down by the time 'kill-server' returns. This causes a problem:
+        #
+        # When I try to start the server again, tmux sees that the server is already running and connects me to the
+        # server that is in the process of being shut down. Then I get disconnected and see an error message saying
+        # 'server exited unexpectedly'.
+        #
+        # To confirm the server is actually down after 'kill-server' returns, I poll the server until I don't get
+        # a response.
         tmux kill-server
+        set max_poll_attempts 5
+        while is_tmux_running
+        and test $max_poll_attempts -gt 0
+            sleep 1
+            set max_poll_attempts (math $max_poll_attempts - 1)
+        end
     end
 
     tmux
