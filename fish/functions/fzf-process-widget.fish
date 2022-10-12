2ddfb32a457f724381a9b23b25de1c8e2b8acc72
Biggie Emmanuel <hi@bigo.lu>
2022-10-12 17:04:45 -0400

    (fzf-process) tweaks

--- fish/functions/fzf-process-widget.fish
+++ fish/functions/fzf-process-widget.fish
@@ -1,5 +1,5 @@
 function fzf-process-widget --description 'Manage processes'
-  set reload_command 'date; ps -e --format user,pid,ppid,nice=NICE,start_time,etime,command --sort=-start_time'
+  set reload_command 'echo "Last Refresh: $(date)"; ps -e --format user,pid,ppid,nice=NICE,start_time,etime,command --sort=-start_time'
   set choice \
       ( \
         FZF_DEFAULT_COMMAND="$reload_command" \
@@ -10,13 +10,15 @@ function fzf-process-widget --description 'Manage processes'
             --no-clear \
             # only search on PID, PPID, and the command
             --nth '2,3,7..' \
-            --bind "change:first,ctrl-alt-r:reload($reload_command)+first,alt-enter:toggle" \
+            --bind "change:first,ctrl-alt-r:reload@$reload_command@+first,alt-enter:toggle" \
             --header-lines=2 \
             --prompt 'processes: ' \
             # I 'echo' the fzf placeholder in the grep regex to get around the fact that fzf substitutions are single quoted and the quotes
             # would mess up the grep regex.
             --preview 'ps --pid {2} >/dev/null; or begin; echo "There is no running process with this ID."; exit; end; echo -s (set_color brwhite) {} (set_color normal); pstree --hide-threads --long --show-pids --unicode --show-parents --arguments {2} | GREP_COLORS="ms=00;36" grep --color=always --extended-regexp --regexp "[^└|─]+,$(echo {2})( .*|\$)" --regexp "^"' \
             --tiebreak=chunk,begin,end \
+            --no-hscroll \
+            --preview-window 'nowrap' \
       )
   or begin
     # necessary since I'm using the --no-clear option in fzf
@@ -38,6 +40,7 @@ function fzf-process-widget --description 'Manage processes'
             --bind "change:first" \
             --header 'Select a signal to send or exit to print the PIDs' \
             --prompt 'signals: ' \
+            --preview '' \
       )
   or begin
     printf %s\n $process_ids
