2359dd01d6c8e3cb9b16382622b7b723f8577353
Biggie Emmanuel <hi@bigo.lu>
2022-10-16 10:40:50 -0400

    (fzf) style change

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -16,7 +16,7 @@ set --global --export FZF_DEFAULT_OPTS "
     --bind 'tab:down,shift-tab:up,ctrl-j:preview-down,ctrl-k:preview-up,change:first,alt-o:change-preview-window(right,60%|bottom,75%)+refresh-preview,ctrl-/:preview(fzf-help-preview)+preview-top,ctrl-\\:refresh-preview,enter:select+accept,ctrl-r:refresh-preview,ctrl-w:toggle-preview-wrap'
     --layout=reverse
     --border=none
-    --color='16,fg:dim,fg+:-1:regular::bold,bg+:-1,info:15,gutter:8,pointer:6:bold,prompt:6:regular,border:15:dim,query:-1:regular,marker:6:bold,header:15,spinner:yellow,hl:cyan:dim,hl+:regular:cyan::bold'
+    --color='16,fg:dim,fg+:-1:regular:underline,bg+:-1,info:15,gutter:8,pointer:-1:bold,prompt:6:regular,border:15:dim,query:-1:regular,marker:-1:bold,header:15,spinner:yellow,hl:cyan:dim,hl+:regular:cyan:underline'
     --margin=3%
     --height 100%
     --prompt='$prompt'
