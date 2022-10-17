1680d2f38e6df7cc22a73870bfd1b87a20e701d1
Biggie Emmanuel <hi@bigo.lu>
2022-10-17 10:39:11 -0400

    (hook) check asdf for updates first

--- install/update_tools.sh
+++ install/update_tools.sh
@@ -57,6 +57,16 @@ banner_underline="$(printf %40s '' | sed 's/ /─/g')"
 printf "%b\n%s\n%s\n%b" "$blue" "$banner_messsage" "$banner_underline" "$reset"
 printf "Checking to see if any actions should be taken as a result of the merge:\n\n"
 
+# NOTE: This should be the first check made because if the version for a tool changes to a version we do not have,
+# that tool will error out when you call it. This could be an issue because some of the checks below may depend on
+# these tools.
+tool_versions='asdf/tool-versions'
+if has_changes "$tool_versions"; then
+  if confirm "Changes have been made to asdf tool version file, would you like asdf to update from it?"; then
+    suppress_error asdf install
+  fi
+fi
+
 if has_added_file || has_deleted_file; then
   if confirm 'A file has been added and/or deleted, would you like dotbot to relink?'; then
     suppress_error ./install/install --only clean link
@@ -81,13 +91,6 @@ vim_plug_snapshot='neovim/vim-plug-snapshot.vim'
 if has_changes "$vim_plug_snapshot"; then
   if confirm "Changes have been made to $vim_plug_snapshot would you like neovim to update from it?"; then
     suppress_error nvim -c 'autocmd VimEnter * PlugRestore'
-  fi
-fi
-
-tool_versions='asdf/tool_versions'
-if has_changes "$tool_versions"; then
-  if confirm "Changes have been made to $tool_versions would you like asdf to update from it?"; then
-    suppress_error asdf install
   fi
 fi
 
