9b3d2e0a9c4704225d27f9619ad794f97bb20ce1
Biggie Emmanuel <hi@bigo.lu>
2022-10-31 19:51:47 -0400

    (firefox) add firefox developer edition

---
+++ firefox/install.sh
@@ -0,0 +1,18 @@
+# shellcheck shell=sh
+
+# Exit if a command returns a non-zero exit code
+set -o errexit
+
+# Exit if an unset variable is referenced
+set -o nounset
+
+cd /tmp
+
+curl --location "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" \
+  | tar --extract --verbose --preserve-permissions --bzip2
+
+sudo mv firefox /opt/
+
+ln -s /opt/firefox/firefox ~/.local/bin/firefox-dev
+
+xdg-settings set default-web-browser my-firefox.desktop
