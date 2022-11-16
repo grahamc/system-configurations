1cbe2939851409f07ac7a52cbe939e61f95a9ba3
Biggie Emmanuel <hi@bigo.lu>
2022-11-16 11:30:36 -0500

    (ipython) make sure ipython belongs to the current python installation

--- fish/conf.d/prefer-ipython-to-python.fish
+++ fish/conf.d/prefer-ipython-to-python.fish
@@ -4,6 +4,8 @@ end
 
 function python --wraps python
     if type --query ipython
+    # Make sure ipython actually belongs to the current python installation
+    and command python -c 'from importlib import util; import sys; sys.exit(0 if util.find_spec("ipython") else 1)'
         if test (count $argv) -eq 0
         or contains -- '-i' $argv
             ipython $argv
