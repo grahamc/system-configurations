c3fead93a377074c96db145b572f118a274991fe
Biggie Emmanuel <hi@bigo.lu>
2023-01-08 12:03:45 -0500

    add a check for fish

--- install/update_tools.sh
+++ install/update_tools.sh
@@ -126,3 +126,11 @@ if has_changes 'bash/profile'; then
   read _unused
   stty "$stty_original"
 fi
+
+fish_function_pattern='fish/functions/*'
+fish_conf_pattern='fish/conf.d/*'
+if has_changes "$fish_function_pattern" || has_changes "$fish_conf_pattern"; then
+  if confirm "A fish configuration or function has changed, would you like to reload all fish shells?"; then
+    suppress_error fish -c 'set --universal _fish_reload_indicator (random)'
+  fi
+fi
