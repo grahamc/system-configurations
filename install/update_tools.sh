c01016214e1ae2339c1150f844177fb6956b5fac
Biggie Emmanuel <hi@bigo.lu>
2023-01-14 16:27:05 -0500

    (update hook) quit neovim after updating it

--- install/update_tools.sh
+++ install/update_tools.sh
@@ -85,7 +85,7 @@ fi
 vim_plug_snapshot='neovim/vim-plug-snapshot.vim'
 if has_changes "$vim_plug_snapshot"; then
   if confirm "The neovim plugin snapshot has changed, would you like neovim to update from it?"; then
-    suppress_error nvim -c 'autocmd VimEnter * PlugRestore'
+    suppress_error nvim -c 'autocmd VimEnter * PlugRestore' -c 'autocmd VimEnter * qall'
   fi
 fi
 
