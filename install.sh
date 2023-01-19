7f0650841be15c40ad81614a01308ad5b15e94bd
Biggie Emmanuel <hi@bigo.lu>
2023-01-19 14:24:38 -0500

    put the SHELL environment variable in the dotbot installer

--- install.sh
+++ install.sh
@@ -11,10 +11,7 @@ if ! [ -d ~/.dotfiles ]; then
 fi
 cd ~/.dotfiles
 
-# Run Dotbot.
-#
-# The shell commands that I run with Dotbot are in POSIX shell so I need to set my $SHELL to it.
-SH_EXECUTABLE="$(command -v sh)"
-SHELL="$SH_EXECUTABLE" ./install/install
+# Run Dotbot
+./install/install
 
 printf 'Finished!\n\nNOTE: The current login session doesn'\''t have the settings in the newly linked login shell profile so you should relogin to apply those settings. Otherwise, you'\''ll have issues like not being able to use the tools installed by homebrew, nix, etc.\n'
