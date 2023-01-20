f71ed7fa9c61d21417ed907e5101e75c5f41b423
Biggie Emmanuel <hi@bigo.lu>
2023-01-20 06:18:43 -0500

    (fish) fix: nix install flag regex

--- fish/functions/nix-env.fish
+++ fish/functions/nix-env.fish
@@ -2,7 +2,10 @@ function nix-env
     command nix-env $argv
     set exit_code $status
 
-    if string match --quiet -- '-*i*' $argv
+    # matches:
+    # a set of short flags containing 'i' or 'e' (e.g. -iA)
+    # '--install' or '--uninstall'
+    if string match --quiet --regex -- '^-([^-]*[ie].*|-((un)?install))$' $argv
         nix-env --query > ~/.config/nix/nix-packages.txt
     end
 
