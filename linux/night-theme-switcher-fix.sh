9febf3888ad81ca7954290b047d37304da94144c
Biggie Emmanuel <hi@bigo.lu>
2023-03-18 22:39:01 -0400

    (linux) fix: make sure GNOME shell theme stays in sync with the GTK theme
    Sometimes the night theme switching extension won't switch both. This usually happens when light/dark mode gets activated and the screen isn't unlocked.

---
+++ linux/night-theme-switcher-fix.sh
@@ -0,0 +1,10 @@
+#!/bin/env sh
+
+dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" | \
+( while true; do
+    read -r X;
+    if echo "$X" | grep "boolean false" 1>/dev/null  2>&1; then
+      gtk_theme="$(dconf read /org/gnome/desktop/interface/gtk-theme)"
+      dconf write /org/gnome/desktop/wm/preferences/theme "$gtk_theme"
+    fi
+done )
