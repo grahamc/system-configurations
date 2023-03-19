ce516aab81e462cb65b2354b08697f68f74c05e7
Biggie Emmanuel <hi@bigo.lu>
2023-03-19 10:00:36 -0400

    (Linux) fix: theme syncer
    - Add logging
    - A desktop entry so it can be autostarted (executables don't work in `~/.config/autostart`)

--- linux/night-theme-switcher-fix.sh
+++ linux/night-theme-switcher-fix.sh
@@ -6,5 +6,6 @@ dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" | \
     if echo "$X" | grep "boolean false" 1>/dev/null  2>&1; then
       gtk_theme="$(dconf read /org/gnome/desktop/interface/gtk-theme)"
       dconf write /org/gnome/desktop/wm/preferences/theme "$gtk_theme"
+      echo 'GNOME shell theme changed.'
     fi
 done )
