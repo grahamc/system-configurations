8bbf2ed04d6f64e36032bb115b157de08202b9f3
Biggie Emmanuel <hi@bigo.lu>
2023-03-20 13:29:38 -0400

    fix key name

--- linux/night-theme-switcher-fix.sh
+++ linux/night-theme-switcher-fix.sh
@@ -5,7 +5,7 @@ dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" | \
     read -r X;
     if echo "$X" | grep "boolean false" 1>/dev/null  2>&1; then
       gtk_theme="$(dconf read /org/gnome/desktop/interface/gtk-theme)"
-      dconf write /org/gnome/desktop/wm/preferences/theme "$gtk_theme"
-      echo 'GNOME shell theme changed.'
+      dconf write /org/gnome/shell/extensions/user-theme/name "$gtk_theme"
+      echo "GNOME shell theme changed to '$gtk_theme'."
     fi
 done )
