d9b258da9a641e6ce4fbf1e64f6641d6c94d1cc4
Biggie Emmanuel <hi@bigo.lu>
2023-03-30 20:03:44 -0400

    get correct theme straight from night theme switcher extension

--- linux/night-theme-switcher-fix.sh
+++ linux/night-theme-switcher-fix.sh
@@ -4,8 +4,8 @@ dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" | \
 ( while true; do
     read -r X;
     if echo "$X" | grep "boolean false" 1>/dev/null  2>&1; then
-      gtk_theme="$(dconf read /org/gnome/desktop/interface/gtk-theme)"
-      dconf write /org/gnome/shell/extensions/user-theme/name "$gtk_theme"
-      echo "GNOME shell theme changed to '$gtk_theme'."
+      night_switcher_shell_theme="$(dconf read /org/gnome/shell/extensions/nightthemeswitcher/shell-variants/night)"
+      dconf write /org/gnome/shell/extensions/user-theme/name "$night_switcher_shell_theme"
+      echo "GNOME shell theme changed to '$night_switcher_shell_theme'."
     fi
 done )
