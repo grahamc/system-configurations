0e00bbab6ba610b1affdac1e79ebe67fd662bf96
Biggie Emmanuel <hi@bigo.lu>
2023-04-10 21:35:05 -0400

    (wezterm) fix: link config directly to the dotfiles repo
    Wezterm wasn't reading the config when it went through the Nix store.

--- .meta/modules/unit/wezterm.nix
+++ .meta/modules/unit/wezterm.nix
@@ -2,10 +2,17 @@
   let
     inherit (import ../util.nix {inherit config lib;})
       makeOutOfStoreSymlink
+      runAfterLinkGeneration
       ;
     inherit (specialArgs) isGui;
   in lib.mkIf isGui {
-    home.file = {
-      ".wezterm.lua".source = makeOutOfStoreSymlink "wezterm/wezterm.lua";
-    };
+    # TODO: Wezterm doesn't read my config when I link it using `makeOutOfStoreSymlink`.
+    # For now I'll symlink directly into my dotfiles repo. Might have to do with the fact
+    # that `makeOutOfStoreSymlink` links into the Nix store?
+    home.activation.weztermSetup = runAfterLinkGeneration ''
+      wezterm_config='/home/biggs/.wezterm.lua'
+      if [ ! -f $wezterm_config ]; then
+        ln --symbolic /home/biggs/.dotfiles/wezterm/wezterm.lua $wezterm_config
+      fi
+    '';
   }
