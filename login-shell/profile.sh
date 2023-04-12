8c7d3f8258b0a71ffe3c3446dd2ab0e85ea2437c
Biggie Emmanuel <hi@bigo.lu>
2023-04-12 19:05:27 -0400

    (login-shell) add Go environment variables

--- login-shell/profile.sh
+++ login-shell/profile.sh
@@ -1,12 +1,20 @@
 # shellcheck shell=sh
 
 # Setup nix
-#
-# DUPLICATE: nix-setup
 _nix_profile_setup_script="$HOME/.nix-profile/etc/profile.d/nix.sh"
 if [ -e "$_nix_profile_setup_script" ]; then
   . "$_nix_profile_setup_script"
 fi
+
+# I want to keep a user-specific config so that I can manage it with Nix Home Manager
+export WATCHMAN_CONFIG_FILE="$HOME/.config/watchman/watchman.json"
+
+# I want to keep a user-specific catalog so that I can manage it with Nix Home Manager
+export XML_CATALOG_FILES="/etc/xml/catalog $HOME/.local/share/xml/catalog"
+
+# go
+export GOPATH="$HOME/.local/share/go"
+export PATH="$GOPATH/bin:$PATH"
 
 # Adding this to the PATH since this is where user-specific executables should go, per the
 # XDG Base Directory spec.
@@ -15,12 +23,6 @@ fi
 # NOTE: I keep this at the bottom so that my executables will be found before any others, allowing me to wrap
 # other executables.
 PATH="$HOME/.local/bin:$PATH"
-
-# I want to keep a user-specific config so that I can manage it with Nix Home Manager
-export WATCHMAN_CONFIG_FILE="$HOME/.config/watchman/watchman.json"
-
-# I want to keep a user-specific catalog so that I can manage it with Nix Home Manager
-export XML_CATALOG_FILES="/etc/xml/catalog $HOME/.local/share/xml/catalog"
 
 # switch to fbterm if we are running in a TTY
 # [ "$TERM" = "linux" ] && [[ $- == *i* ]] && command -v fbterm >/dev/null 2>&1 && FBTERM=1 exec fbterm
