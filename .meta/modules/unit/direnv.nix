2f480d84ea2f60b44407a5e47bac6b0e3fdbf051
Biggie Emmanuel <hi@bigo.lu>
2023-04-12 11:40:01 -0400

    (direnv) add nix-direnv

--- .meta/modules/unit/direnv.nix
+++ .meta/modules/unit/direnv.nix
@@ -7,9 +7,15 @@
     {
       home.packages = with pkgs; [
         direnv
+        nix-direnv
       ];
 
       xdg.configFile = {
         "direnv/direnv.toml".source = makeSymlinkToRepo "direnv/direnv.toml";
+        "direnv/direnvrc".source = makeSymlinkToRepo "direnv/direnvrc.bash";
+        # I'm using an out-of-store symlink because since links are generated before programs are installed, if
+        # nix-direnv hasn't been installed yet, a regular symlink would throw an error. For out-of-store links
+        # however, Home Manager doesn't require that the source file exists before making the link.
+        "direnv/lib/use_nix_direnv.sh".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix-profile/share/nix-direnv/direnvrc";
       };
     }
