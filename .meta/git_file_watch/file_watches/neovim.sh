9d41c4a38d2fa40af60ee68bf34ab7c5c51d15bb
Biggie Emmanuel <hi@bigo.lu>
2023-04-18 14:25:07 -0400

    (file-watcher) remove unused file watcher

--- .meta/git_file_watch/file_watches/neovim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-vim_plug_snapshot='neovim/plugfile-lock.vim'
-if has_changes "$vim_plug_snapshot"; then
-  if confirm "The neovim plugin snapshot has changed, would you like neovim to update from it?"; then
-    suppress_error nvim -c 'autocmd VimEnter * PlugRestore' -c 'autocmd VimEnter * qall'
-  fi
-fi
