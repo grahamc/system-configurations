4587f2668e9ff4fde845bff9fb778587dd474bae
Biggie Emmanuel <hi@bigo.lu>
2023-04-25 23:07:58 -0400

    (home-manager) make home-manager use my version of flake-utils

--- flake.nix
+++ flake.nix
@@ -5,7 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        utils.follows = "flake-utils";
+      };
     };
     nix-index-database = {
       url = "github:Mic92/nix-index-database";
