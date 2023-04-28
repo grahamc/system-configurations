fee84883842c136bb8b81b01e6e39425c8c69a53
Biggie Emmanuel <hi@bigo.lu>
2023-04-28 09:59:27 -0400

    unused

--- neovim/lua/terminal.lua
+++ neovim/lua/terminal.lua
@@ -1755,18 +1755,6 @@ Plug(
             )
           )
         end,
-
-        ["prosemd_lsp"] = function()
-          lspconfig.prosemd_lsp.setup(
-            vim.tbl_deep_extend(
-              'force',
-              default_server_config,
-              {
-                filetypes = {'markdown', 'gitcommit', 'text'},
-              }
-            )
-          )
-        end,
       })
     end,
   }
