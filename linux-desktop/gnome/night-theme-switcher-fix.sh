922a3c78dba1cb7567e3b6d0a2745f00e08fda31
Biggie Emmanuel <hi@bigo.lu>
2023-05-02 11:41:59 -0400

    (linux) fix night-theme-switcher

--- linux-desktop/gnome/night-theme-switcher-fix.sh
+++ linux-desktop/gnome/night-theme-switcher-fix.sh
@@ -5,7 +5,11 @@ dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" | \
     read -r X;
     if echo "$X" | grep "boolean false" 1>/dev/null  2>&1; then
       gtk_theme="$(dconf read /org/gnome/desktop/interface/gtk-theme)"
-      dconf write /org/gnome/shell/extensions/user-theme/name "$gtk_theme"
-      echo "GNOME shell theme changed to '$gtk_theme'."
+      shell_theme='Fluent-round-Dark'
+      if echo "$gtk_theme" | grep -q Light; then
+        shell_theme='Fluent-round'
+      fi
+      dconf write /org/gnome/shell/extensions/user-theme/name "$shell_theme"
+      echo "GNOME shell theme changed to '$shell_theme'."
     fi
 done )
