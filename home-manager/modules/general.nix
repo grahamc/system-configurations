eb5792fb8831aa5729daa46c2c7e360eaffc0634
Biggie Emmanuel <hi@bigo.lu>
2023-05-06 08:47:52 -0400

    (home-manager) fix: specify target for symlink

--- home-manager/modules/general.nix
+++ home-manager/modules/general.nix
@@ -4,6 +4,7 @@
       # I link to this directory from other modules so I make sure the key here is unique and specify the target
       # inside the attribute set.
       ".local/bin (general)" = {
+        target = ".local/bin";
         source = "general/executables";
         sourcePath = ../../general/executables;
         recursive = true;
