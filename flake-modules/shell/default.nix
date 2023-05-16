4a61eda272ab84d88e1965e881dcea8592ef93f2
Biggie Emmanuel <hi@bigo.lu>
2023-05-16 11:18:06 -0400

    refactor

--- flake-modules/shell/default.nix
+++ flake-modules/shell/default.nix
@@ -1,5 +1,9 @@
 # This output lets me run my shell environment, both programs and their config files, completely from the nix store.
 # Useful for headless servers or containers.
+#
+# I also bundle this output into an executable (check the GitHub actions for this repository to see how) so there
+# is some configuration added here to try and make the resulting executable smaller. For example, removing
+# dependencies that are particularly large like systemd.
 { inputs, self, ... }:
   {
     perSystem = {lib, system, pkgs, ...}:
@@ -11,6 +15,7 @@
             minimalFish = pkgs.fish.override {
               usePython = false;
             };
+            # By default English is included so that will be the only one in this package.
             minimalLocales = pkgs.glibcLocales.override { allLocales = false; };
             minimalOverlay = final: prev: {
               fish = minimalFish;
@@ -21,59 +26,61 @@
                 glibcLocales = minimalLocales;
               };
             };
+            shellModule = {config, ...}:
+              {
+                # I want a self contained executable so I can't have symlinks that point outside the Nix store.
+                config.repository.symlink.makeCopiesInstead = true;
+                # I do this to override the original link to the treesitter parsers.
+                config.xdg.dataFile."nvim/site/parser".source = lib.mkForce
+                  (pkgs.writeShellApplication {name = "fake"; text = "";});
+                config.programs.nix-index = {
+                  enable = false;
+                  symlinkToCacheHome = false;
+                };
+                # I do this to override the original 'printChanges' activation script so I can remove the dependency
+                # on `nvd`.
+                config.home.activation.printChanges = lib.mkForce "";
+                config.programs.home-manager.enable = lib.mkForce false;
+                # This removes the dependency on `sd-switch`.
+                config.systemd.user.startServices = lib.mkForce "suggest";
+                # There is no option to prevent Home Manager from making this environment variables and overriding
+                # glibcLocales in an overlay would cause too many rebuild so instead I overwrite the environment
+                # variables. Now, glibcLocales won't be a dependency.
+                config.home.sessionVariables = lib.mkForce {
+                  LOCALE_ARCHIVE_2_27 = "";
+                  LOCALE_ARCHIVE_2_11 = "";
+                };
+              };
             homeManagerOutput = self.lib.makeFlakeOutput system {
               inherit hostName;
-              # To remove the systemd dependency
+              # I want to remove the systemd dependency, but there is no option for that. Instead, I set the user
+              # to root since Home Manager won't include systemd if the user is root.
+              # see: https://github.com/nix-community/home-manager/blob/master/modules/systemd.nix
               username = "root";
               modules = with self.lib.modules; [
                 profile.system-administration
-                ({config, ...}: {
-                  # I want a self contained executable so I can't have symlinks that point outside the Nix store.
-                  config.repository.symlink.makeCopiesInstead = true;
-                  config.programs.fish.package = minimalFish;
-                  config.xdg.dataFile."nvim/site/parser".source = lib.mkForce
-                    (pkgs.writeShellApplication {name = "fake"; text = "";});
-                  config.programs.nix-index = {
-                    enable = false;
-                    symlinkToCacheHome = false;
-                  };
-                  config.home.activation.printChanges = lib.mkForce "";
-                  config.programs.home-manager.enable = lib.mkForce false;
-                  config.systemd.user.startServices = lib.mkForce "suggest";
-                  config.home.sessionVariables = lib.mkForce {
-                    LOCALE_ARCHIVE_2_27 = "";
-                    LOCALE_ARCHIVE_2_11 = "";
-                  };
-                })
+                shellModule
               ];
               isGui = false;
               overlays = [minimalOverlay];
             };
-            inherit (homeManagerOutput.legacyPackages.homeConfigurations.${hostName}) activationPackage;
-            # I don't want the programs that this script depends on to be in the $PATH since they are not
-            # necessarily part of my Home Manager configuration so I'll set them to variables instead.
-            coreutilsBinaryPath = "${pkgs.coreutils}/bin";
-            mktemp = "${coreutilsBinaryPath}/mktemp";
-            copy = "${coreutilsBinaryPath}/cp";
-            chmod = "${coreutilsBinaryPath}/chmod";
-            basename = "${coreutilsBinaryPath}/basename";
-            which = "${pkgs.which}/bin/which";
-            foreignEnvFunctionPath = "${pkgs.fishPlugins.foreign-env}/share/fish/vendor_functions.d";
-            fish = "${minimalFish}/bin/fish";
             shellBootstrapScriptName = "shell";
+            # Normally, to make a shell script I would use the function `nixpkgs.writeShellApplication` and specify
+            # its dependencies through the attribute `runtimeInputs`. Then those dependencies would be added to the
+            # $PATH before the script executes. In this case, I don't want the programs that the script depends on to
+            # be in the $PATH because I don't want them on the $PATH of the shell that gets launched at the end of the
+            # script. Instead, I'll supply the dependencies through the variables listed below.
             shellBootstrapScript = import ./shell-bootstrap-script.nix
-              {
-                inherit
-                  activationPackage
-                  mktemp
-                  copy
-                  chmod
-                  fish
-                  coreutilsBinaryPath
-                  basename
-                  which
-                  foreignEnvFunctionPath
-                  ;
+              rec {
+                inherit (homeManagerOutput.legacyPackages.homeConfigurations.${hostName}) activationPackage;
+                coreutilsBinaryPath = "${pkgs.coreutils}/bin";
+                mktemp = "${coreutilsBinaryPath}/mktemp";
+                copy = "${coreutilsBinaryPath}/cp";
+                chmod = "${coreutilsBinaryPath}/chmod";
+                basename = "${coreutilsBinaryPath}/basename";
+                fish = "${minimalFish}/bin/fish";
+                which = "${pkgs.which}/bin/which";
+                foreignEnvFunctionPath = "${pkgs.fishPlugins.foreign-env}/share/fish/vendor_functions.d";
                 localeArchive = "${minimalLocales}/lib/locale/locale-archive";
               };
             shellBootstrap = pkgs.writeScriptBin shellBootstrapScriptName shellBootstrapScript;
