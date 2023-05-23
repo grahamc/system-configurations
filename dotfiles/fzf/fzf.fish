eb999fd38fdcf35e44de2eb427c19a0df8b34eb0
Biggie Emmanuel <hi@bigo.lu>
2023-05-23 08:32:16 -0400

    (fzf) don't dim the inactive items, the active item already has an underline

--- dotfiles/fzf/fzf.fish
+++ dotfiles/fzf/fzf.fish
@@ -13,7 +13,7 @@ set --global --export FZF_DEFAULT_OPTS " \
     --bind 'tab:down,shift-tab:up,ctrl-j:preview-down,ctrl-k:preview-up,change:first,ctrl-o:change-preview-window(right,60%|bottom,75%)+refresh-preview,ctrl-/:preview(fzf-help-preview)+preview-top,ctrl-\\:refresh-preview,enter:accept,ctrl-r:refresh-preview,ctrl-w:toggle-preview-wrap,alt-enter:toggle,ctrl-t:track+unbind(change),focus:rebind(change)' \
     --layout=reverse \
     --border=none \
-    --color='16,fg:dim,fg+:-1:regular:underline,bg+:-1,info:15,gutter:8,pointer:-1:bold,prompt:6:regular,border:15:dim,query:-1:regular,marker:-1:bold,header:15,spinner:yellow,hl:cyan:dim,hl+:regular:cyan:underline' \
+    --color='16,fg+:-1:regular:underline,bg+:-1,info:15,gutter:8,pointer:-1:bold,prompt:6:regular,border:15:dim,query:-1:regular,marker:-1:bold,header:15,spinner:yellow,hl:cyan:dim,hl+:regular:cyan:underline' \
     --margin=3% \
     --height 100% \
     --prompt='$_magnifying_glass' \
