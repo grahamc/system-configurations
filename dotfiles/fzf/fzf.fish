c86a2431c5a7d548b3902825bbd3100fb7567b15
Biggie Emmanuel <hi@bigo.lu>
2023-05-24 18:50:52 -0400

    change fzf keybinds

--- dotfiles/fzf/fzf.fish
+++ dotfiles/fzf/fzf.fish
@@ -10,7 +10,7 @@ set _magnifying_glass (echo -s \uf002 '  ')
 set --global --export FZF_DEFAULT_OPTS " \
     --cycle \
     --ellipsis='…' \
-    --bind 'tab:down,shift-tab:up,ctrl-j:preview-down,ctrl-k:preview-up,change:first,ctrl-o:change-preview-window(right,60%|bottom,75%)+refresh-preview,ctrl-/:preview(fzf-help-preview)+preview-top,ctrl-\\:refresh-preview,enter:accept,ctrl-r:refresh-preview,ctrl-w:toggle-preview-wrap,alt-enter:toggle,ctrl-t:track+unbind(change),focus:rebind(change)' \
+    --bind 'tab:down,shift-tab:up,ctrl-j:preview-down,ctrl-k:preview-up,change:first,ctrl-o:change-preview-window(right,60%|bottom,75%)+refresh-preview,ctrl-/:preview(fzf-help-preview)+preview-top,ctrl-\\:refresh-preview,enter:accept,ctrl-r:refresh-preview,ctrl-w:toggle-preview-wrap,alt-enter:toggle,ctrl-t:track+unbind(change),focus:rebind(change),f7:prev-history,f8:next-history,ctrl-p:toggle-preview' \
     --layout=reverse \
     --border=none \
     --color='16,fg+:-1:regular:underline,bg+:-1,info:15,gutter:8,pointer:-1:bold,prompt:6:regular,border:15:dim,query:-1:regular,marker:-1:bold,header:15,spinner:yellow,hl:cyan:dim,hl+:regular:cyan:underline' \
