e8c323a45c009894b7fa046bf0453c984bcb3413
Biggie Emmanuel <hi@bigo.lu>
2023-05-25 13:38:31 -0400

    (firefox) remove .desktop file, it gets updated when firefox updates

--- flake-modules/home-manager/modules/firefox-developer-edition.nix
+++ flake-modules/home-manager/modules/firefox-developer-edition.nix
@@ -5,7 +5,6 @@
   in lib.mkIf (isGui && isLinux) {
     repository.symlink.xdg.dataFile = {
       "applications/my-firefox.desktop".source = "firefox-developer-edition/my-firefox.desktop";
-      "applications/firefox-developer-edition.desktop".source = "firefox-developer-edition/firefox-developer-edition.desktop";
     };
 
     repository.symlink.home.file.".local/bin/my-firefox".source = "firefox-developer-edition/my-firefox";
