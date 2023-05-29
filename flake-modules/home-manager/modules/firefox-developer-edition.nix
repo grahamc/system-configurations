51fdaf44c99e06279c4962717f75d7140302235a
Biggie Emmanuel <hi@bigo.lu>
2023-05-29 10:48:47 -0400

    (firefox) fix: don't try to set the default web browser

--- flake-modules/home-manager/modules/firefox-developer-edition.nix
+++ flake-modules/home-manager/modules/firefox-developer-edition.nix
@@ -3,16 +3,14 @@
     inherit (specialArgs) isGui;
     inherit (pkgs.stdenv) isLinux;
   in lib.mkIf (isGui && isLinux) {
+    # You need to run this command set this as the default web browser:
+    # `xdg-settings set default-web-browser my-firefox.desktop`.
+    # I tried to run it here, but it failed with exit code 2 which, according to the manpage, means it couldn't find a
+    # file. I think this is because home-manager doesn't run as the current user so it can't find the .desktop file
+    # linked below.
     repository.symlink.xdg.dataFile = {
       "applications/my-firefox.desktop".source = "firefox-developer-edition/my-firefox.desktop";
     };
 
     repository.symlink.home.file.".local/bin/my-firefox".source = "firefox-developer-edition/my-firefox";
-
-    home.activation.firefoxDeveloperEditionSetup = lib.hm.dag.entryAfter
-      # Must be after `linkGeneration` since that's when the desktop entry will be linked in.
-      ["linkGeneration"]
-      ''
-        ${pkgs.xdg-utils}/bin/xdg-settings set default-web-browser my-firefox.desktop
-      '';
   }
