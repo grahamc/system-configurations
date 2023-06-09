1da25de6c5faa46cbb38444c5a8a5ba2e12e1c45
Biggie Emmanuel <hi@bigo.lu>
2023-06-09 14:24:58 -0400

    (bat) show filename in header

--- dotfiles/fish/conf.d/fuzzy-widgets.fish
+++ dotfiles/fish/conf.d/fuzzy-widgets.fish
@@ -14,11 +14,11 @@ function fzf-grep-widget --description 'Search by line, recursively, from curren
             --bind "ctrl-e:execute(nvim '+call cursor({2},{3})' {1} < /dev/tty > /dev/tty 2>&1)+refresh-preview,change:first+reload:sleep 0.1; $rg_command {q} || true" \
             --delimiter ':' \
             --prompt 'lines: ' \
-            --preview-window '+{2}/3,75%,~1' \
+            --preview-window '+{2}/3,75%,~2' \
             # the minus 2 prevents a weird line wrap issue
             # The head and tail commands are there to remove the first and last line of output of bat
             # i.e. the top and bottom border of bat since I don't like how they look
-            --preview 'echo -s (set_color brwhite) {1} \n; bat --paging=never --terminal-width (math $FZF_PREVIEW_COLUMNS - 2) {1} --highlight-line {2} | tail -n +2 | head -n -1' \
+            --preview 'bat --paging=never --terminal-width (math $FZF_PREVIEW_COLUMNS - 2) {1} --highlight-line {2} | tail -n +2 | head -n -1' \
       )
   or return
 
@@ -146,7 +146,6 @@ function my-fzf-file-widget --description 'Search files'
   # - The head and tail commands are there to remove the first and last line of output of bat i.e. the top and bottom
   # border of bat since I don't like how they look
   set preview_command '
-  echo -s (set_color brwhite) {} \n
   if file --brief --mime-type {} | grep -q -i image
     if set --query TMUX
       timg --center -g "$FZF_PREVIEW_COLUMNS"x"$FZF_PREVIEW_LINES" -p quarter {}
@@ -168,7 +167,7 @@ function my-fzf-file-widget --description 'Search files'
         fzf-tmux-zoom \
             --prompt "$prompt" \
             --preview "$preview_command" \
-            --preview-window '75%,~1' \
+            --preview-window '75%,~2' \
       )
   or return
 
@@ -204,8 +203,8 @@ function fzf-directory-widget --description 'Seach directories'
         FZF_DEFAULT_COMMAND="test '$dir' = '.' && set _args '--strip-cwd-prefix' || set _args '.' '$dir'; fd \$_args --follow --hidden --type directory --type symlink" \
         fzf-tmux-zoom \
             --prompt "$prompt" \
-            --preview 'echo -s (set_color brwhite) {} \n; lsd {}' \
-            --preview-window '75%,~1' \
+            --preview 'echo -s {} \n (set_color brwhite)(string repeat --count $FZF_PREVIEW_COLUMNS ─); lsd {}' \
+            --preview-window '75%,~2' \
             --keep-right \
       )
   or return
