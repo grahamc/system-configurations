c3e264abceae27da14d10c08f5455e5997f7fce8
Biggie Emmanuel <hi@bigo.lu>
2023-10-23 13:17:07 -0400

    (nix,terminal) include fonts

--- flake-modules/shell/default.nix
+++ flake-modules/shell/default.nix
@@ -104,7 +104,7 @@
               set -o errexit
               set -o nounset
               set -o pipefail
-              exec ${pkgs.wezterm}/bin/wezterm --config-file ${inputs.self.outPath}/dotfiles/wezterm/wezterm.lua start -- ${shellBootstrap}/bin/${shellBootstrapScriptName}
+              exec ${pkgs.wezterm}/bin/wezterm --config-file ${inputs.self.outPath}/dotfiles/wezterm/wezterm.lua --config 'font_locator="ConfigDirsOnly"' --config 'font_dirs={"${(import inputs.nixpkgs {inherit system; overlays = [self.overlays.default];}).myFonts}"}' start -- ${shellBootstrap}/bin/${shellBootstrapScriptName}
             '';
           in
             {
