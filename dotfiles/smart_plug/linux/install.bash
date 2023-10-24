47d78e493133a85ca1b32a33ae593459856fb065
Biggie Emmanuel <hi@bigo.lu>
2023-10-24 13:07:18 -0400

    (smart-plug) linux fixes
    - when installing don't make backups if files already exist. One because I have everything under version control and more importantly because NetworkManager would run the backups before shutting down as well because unlike systemd services, NetworkManager pre-down scripts don't have to be enabled, anything in that directory gets run.
    - Instead of stalling the NetworkManager for a second, just turn my speakers off. Now there is no race condition.

--- dotfiles/smart_plug/linux/install.bash
+++ dotfiles/smart_plug/linux/install.bash
@@ -3,7 +3,7 @@
 # exit the script if any command returns a non-zero exit code
 set -e
 
-sudo install --compare --backup --suffix=.bak --owner=root --group=root --mode='u=rwx,g=r,o=r' -D --verbose --no-target-directory ~/.dotfiles/dotfiles/smart_plug/linux/wait-one-second /etc/NetworkManager/dispatcher.d/pre-down.d/wait-one-second
-sudo install --compare --backup --suffix=.bak --owner=root --group=root --mode='u=rwx,g=r,o=r' -D --verbose --no-target-directory ~/.dotfiles/dotfiles/smart_plug/linux/smart-plug-daemon.service /etc/systemd/system/smart-plug-daemon.service
+sudo install --compare --owner=root --group=root --mode='u=rwx,g=r,o=r' -D --verbose --no-target-directory ~/.dotfiles/dotfiles/smart_plug/linux/turn-off-speakers /etc/NetworkManager/dispatcher.d/pre-down.d/turn-off-speakers
+sudo install --compare --owner=root --group=root --mode='u=rwx,g=r,o=r' -D --verbose --no-target-directory ~/.dotfiles/dotfiles/smart_plug/linux/smart-plug-daemon.service /etc/systemd/system/smart-plug-daemon.service
 sudo systemctl enable smart-plug-daemon.service
 sudo systemctl start smart-plug-daemon.service
