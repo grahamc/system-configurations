b985dec35b765e6ac29ee568a0a31fd86cf29dbe
Biggie Emmanuel <hi@bigo.lu>
2023-10-23 21:12:37 -0400

    (smart-plug) linux fixes
    - enter the linux/ directory before executing the script, otherwise the smart-plug.py import doesn't work
    - use exec when running the python script so the python script is at the root of the cgroup for the systemd service. Without this, my signal handlers in the script were not working properly. I think that's because systemd only sends SIGTERM to the root process in a cgroup when you stop a service.

--- dotfiles/smart_plug/flake.nix
+++ dotfiles/smart_plug/flake.nix
@@ -26,7 +26,14 @@
           pythonWithPackages = pkgs.python3.withPackages pythonPackages;
           scriptText = if pkgs.stdenv.isLinux
             then ''
-              python ${self}/linux/smart-plug-daemon.py
+              # Not exactly sure why, but the script only works when I'm in this directory. Probably something to do with
+              # how I'm importing smart_plug.py
+              cd ${self}/linux
+              # I'm using exec so the python process will be the root process in the cgroup for the systemd service
+              # and not this shell. This way it will receive any signals sent by systemd, for example SIGTERM
+              # when I stop the service. Without exec, the SIGTERM listener in the python script wasn't working
+              # properly.
+              exec python smart-plug-daemon.py
             ''
             else ''
               python ${self}/smart_plug.py "$@"
