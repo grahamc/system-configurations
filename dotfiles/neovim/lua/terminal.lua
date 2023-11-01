e0afdaef7b44e2522239cab4a711a04a9a1662bb
Biggie Emmanuel <hi@bigo.lu>
2023-10-31 20:59:55 -0400

    (neovim) make vim-signify motions wrap around

--- dotfiles/neovim/lua/terminal.lua
+++ dotfiles/neovim/lua/terminal.lua
@@ -1065,7 +1065,42 @@ vim.api.nvim_create_autocmd(
 
 -- Miscellaneous {{{
 -- Add icons to the gutter to represent version control changes (e.g. new lines, modified lines, etc.)
-Plug('mhinz/vim-signify')
+Plug(
+  'mhinz/vim-signify',
+  {
+    config = function()
+      -- Make [c and ]c wrap around. Taken from here:
+      -- https://github.com/mhinz/vim-signify/issues/239#issuecomment-305499283
+      vim.cmd([[
+        function! s:signify_hunk_next(count) abort
+          let oldpos = getcurpos()
+          call sy#jump#next_hunk(a:count)
+          if getcurpos() == oldpos
+            call sy#jump#prev_hunk(9999)
+          endif
+        endfunction
+
+        function! s:signify_hunk_prev(count) abort
+          let oldpos = getcurpos()
+          call sy#jump#prev_hunk(a:count)
+          if getcurpos() == oldpos
+            call sy#jump#next_hunk(9999)
+          endif
+        endfunction
+
+        nnoremap <silent> <expr> <plug>(sy-hunk-next) &diff
+              \ ? ']c'
+              \ : ":\<c-u>call <sid>signify_hunk_next(v:count1)\<cr>"
+        nnoremap <silent> <expr> <plug>(sy-hunk-prev) &diff
+              \ ? '[c'
+              \ : ":\<c-u>call <sid>signify_hunk_prev(v:count1)\<cr>"
+
+        nmap ]c <plug>(sy-hunk-next)
+        nmap [c <plug>(sy-hunk-prev)
+      ]])
+    end
+  }
+)
 -- I'm setting all of these so that the signify signs will be added to the sign column, but NOT be visible. I don't
 -- want them to be visible because I already change the color of my statuscolumn border to indicate git changes. I want
 -- them to be added to the sign column so I know where to color my statuscolumn border.
