2aed34e0a0324300d9d608dbe47268694bbfbd6e
Biggie Emmanuel <hi@bigo.lu>
2023-11-08 04:44:43 -0500

    fix(login-shell, zsh, vscode): make sure we're connected to a terminal before `exec`ing into `fish`
    vscode is launching zsh in interactive mode to set the environment for the window and when I `exec` that seems to break something so my environment doesn't get setup properly

--- dotfiles/login-shell/zshrc.zsh
+++ dotfiles/login-shell/zshrc.zsh
@@ -1,1 +1,4 @@
-[[ -o interactive ]] && [ "$(basename "$SHELL")" != 'fish' ] && command -v fish >/dev/null 2>&1 && SHELL="$(command -v fish)" exec fish
+# The `-t` checks are to make sure that we are at a terminal because as part of vscode's shell resolution it launches
+# the shell in interactive mode and then I call `exec` which seems to break things so my environment variables don't
+# get setup properly.
+[[ -o interactive ]] && [ -t 0 ] && [ -t 1 ] && [ -t 2 ] && [ "$(basename "$SHELL")" != 'fish' ] && command -v fish >/dev/null 2>&1 && SHELL="$(command -v fish)" exec fish
