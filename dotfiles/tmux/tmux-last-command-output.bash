0a51fa7df1a1ba03850a600eb24d93e77fd89fb0
Biggie Emmanuel <hi@bigo.lu>
2023-11-11 20:38:30 -0500

    fix(tmux, fish): use a zero-width character to mark the prompt so it doesn't add extra space

--- dotfiles/tmux/tmux-last-command-output.bash
+++ dotfiles/tmux/tmux-last-command-output.bash
@@ -4,10 +4,10 @@
 # https://github.com/artemave/tmux_capture_last_command_output/blob/bd2cca21bc32c2d6652d7b6fdc36cd61409ddd73/plugin.sh
 
 pane_contents=$(tmux capture-pane -e -p -S '-' -J)
-# \u00A0 is a non-breaking space. I use it to determine where my prompt starts and ends so I can view the
-# output of the last command. (see: tmux.fish for how it gets added to my prompt).
-prompt_pattern="$(printf '\u00A0')"
-last_command_output=$(echo "$pane_contents" | tac | sed -e "0,/$prompt_pattern/d" | sed -e "0,/$prompt_pattern/d" | sed "/$prompt_pattern/,\$d" | tac)
+# I use this zero-width char to mark the start/end of my prompt. This way I can extract the last command's output
+# in here. (see: tmux.fish for how I mark my prompt).
+zero_width_char="$(printf '\u200D')"
+last_command_output=$(echo "$pane_contents" | tac | sed -e "0,/$zero_width_char/d" | sed -e "0,/$zero_width_char/d" | sed "/$zero_width_char/,\$d" | tac)
 
 if ! choice="$(printf "%s\n" less fzf nvim | fzf --prompt 'command output viewer: ' --no-preview --height ~100% --margin 0,2,0,2 --border rounded)"; then
   exit
