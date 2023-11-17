3384adee1c87926402123837e3e1ef3af4c732e6
Biggie Emmanuel <code@bigo.lu>
2023-11-17 11:21:54 -0500

    fix(neovim): remove cache when I change nix generations

--- flake-modules/home-manager/modules/neovim.nix
+++ flake-modules/home-manager/modules/neovim.nix
@@ -12,6 +12,25 @@
     };
 
     vimPlug.configDirectory = config.repository.directoryPath + "/dotfiles/neovim/lua"; 
+
+    # TODO: vim.loader() uses modification time and file size as a cache key. This is a problem for the plugin
+    # sqlite.lua because in one of its files (defs.lua) has the path to sqlite which changes when I upgrade sqlite.
+    # The problem is that nix sets the modification time to the epoch for all files and the size will be the same
+    # because (I think) the hashes that nix generates for package paths are all the same length, only the letters in
+    # the hash change. This means when I start vim after sqlite has been upgraded, sqlite.lua won't be able to find
+    # sqlite (assuming the old version has been garbage collected already) because neovim is still using the stale,
+    # cached bytecode for defs.lua which contains the old path to sqlite.
+    home.activation.vimLoaderFix = lib.hm.dag.entryAfter
+      [ "writeBoundary" ]
+      # Read this to see why the `tr` command is needed:
+      # https://stackoverflow.com/questions/16739300/redirect-ex-command-to-stdout-in-vim
+      #
+      # The `grep` command is there to filter out any messages that get printed on startup.
+      # I'm redirecting stderr to stdout because neovim prints its output on stderr.
+      # The '*' is outside the string so bash will use it as a regex matcher and not a literal '*'.
+      ''
+        rm -rf "''$(${pkgs.neovim-unwrapped}/bin/nvim --headless -c 'lua= vim.fn.stdpath("cache")' -c 'quit' 2>&1 | tr -d '\r' | grep -E '^/')/luac/"*
+      '';
     
     home.file = {
       "${config.repository.directory}/.luarc.json".source =
@@ -26,7 +45,8 @@
                   # Read this to see why the `tr` command is needed:
                   # https://stackoverflow.com/questions/16739300/redirect-ex-command-to-stdout-in-vim
                   #
-                  # The `grep` command is there to filter out any messages that get printed on startup.
+                  # The command is there to filter out any messages that get printed on startup.
+                  # I'm redirecting stderr to stdout because neovim prints its output on stderr.
                   ''
                     ${pkgs.neovim-unwrapped}/bin/nvim --headless  -c 'lua for _,directory in ipairs(vim.api.nvim_get_runtime_file("", true)) do print(directory) end' -c 'quit' 2>&1 | tr -d '\r' | grep -E '^/' > $out
                   ''
