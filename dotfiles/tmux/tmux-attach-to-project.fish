a3f3d5661e746ef67d679c762166b66f756f075a
Biggie Emmanuel <code@bigo.lu>
2023-11-25 20:46:52 -0500

    (tmux) don't truncate directory segments when creating a name for a project session

--- dotfiles/tmux/tmux-attach-to-project.fish
+++ dotfiles/tmux/tmux-attach-to-project.fish
@@ -2,7 +2,9 @@
 
 # Some characters can't be used in a session name so I'll substitute them the same way tmux would if I were to pass
 # the original name to `tmux new-session -s <original_name>`.
-set session_name (prompt_pwd | string replace --all '.' '_')
+#
+# Setting `--dir-length` to 0 disables path segment shortening.
+set session_name (prompt_pwd --dir-length 0 | string replace --all '.' '_')
 
 if not tmux attach-session -t "$session_name"
   tmux new-session -s "$session_name"
