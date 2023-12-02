2d3471398cb8646e6b9166daf2a07b1861dc9482
Biggie Emmanuel <code@bigo.lu>
2023-12-01 20:11:41 -0500

    (nix) include homepage in `nix-info`

--- dotfiles/nix/nix-info.fish
+++ dotfiles/nix/nix-info.fish
@@ -7,4 +7,4 @@
 # longDescription.
 #
 # Using the newline in the printf format so we have one trailing newline to end the entire output.
-printf "%s\n" "$(nix-env -qaA "nixpkgs."$argv --json --meta 2>/dev/null | jq -r '.[] | .name + " " + .meta.description, "", (.meta.longDescription // "" | rtrimstr("\n"))')"
+printf "%s\n" "$(nix-env -qaA "nixpkgs."$argv --json --meta 2>/dev/null | jq -r '.[] | .name + " " + .meta.description, .meta.homepage, "", (.meta.longDescription // "" | rtrimstr("\n"))')"
