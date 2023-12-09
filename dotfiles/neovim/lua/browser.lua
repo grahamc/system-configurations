d493804452860bc9414e98db086958be9992f806
Biggie Emmanuel <code@bigo.lu>
2023-12-08 21:38:43 -0500

    fix(neovim): firenvim firenvim settings need to be set before the plugin load so I can't do it in a config function.

--- dotfiles/neovim/lua/browser.lua
+++ dotfiles/neovim/lua/browser.lua
@@ -6,25 +6,22 @@ end
 -- disable statusline
 vim.o.laststatus = 0
 
-vim.keymap.set('n', '<C-x>', '<Cmd>wq<CR>')
+-- Before quitting, I move the cursor one to the right so the cursor will be in front of the current character
+-- after quitting neovim instead of behind it. I use `a` instead of `l` to account for when the cursor is on the last
+-- character of the line.
+vim.keymap.set('n', '<M-e>', 'a<Cmd>wq<CR>')
 
-Plug(
-  'glacambre/firenvim',
-  {
-    config = function()
-      vim.g.firenvim_config =  {
-        localSettings = {
-          ['.*'] = {
-            -- Don't automatically load firenvim in text areas, I'll do it manually with a key bind
-            takeover = 'never',
-            -- Use firenvim's commandline instead of neovim's
-            cmdline = 'firenvim',
-            -- A second after the cursor moves, hide the commandline. This is a workaround for a bug:
-            -- https://github.com/glacambre/firenvim#configuring-message-timeout
-            cmdlineTimeout = 1000,
-          },
-        },
-      }
-    end,
-  }
-)
+Plug('glacambre/firenvim')
+vim.g.firenvim_config =  {
+  localSettings = {
+    ['.*'] = {
+      -- Don't automatically load firenvim in text areas, I'll do it manually with a key bind
+      takeover = 'never',
+      -- Use firenvim's commandline instead of neovim's
+      cmdline = 'firenvim',
+      -- A second after the cursor moves, hide the commandline. This is a workaround for a bug:
+      -- https://github.com/glacambre/firenvim#configuring-message-timeout
+      cmdlineTimeout = 1000,
+    },
+  },
+}
