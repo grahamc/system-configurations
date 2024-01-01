6dfcb999fca9c68723db491b9f48023678f7dd0b
Biggie Emmanuel <code@bigo.lu>
2024-01-01 04:22:00 -0500

    (tmux) exit script if not in tmux

--- dotfiles/tmux/tmux-last-command-output.bash
+++ dotfiles/tmux/tmux-last-command-output.bash
@@ -7,6 +7,10 @@ set -o pipefail
 # Logic adapted from here:
 # https://github.com/artemave/tmux_capture_last_command_output/blob/bd2cca21bc32c2d6652d7b6fdc36cd61409ddd73/plugin.sh
 
+if [ -z "${TMUX:-}" ]; then
+  exit
+fi
+
 pane_contents=$(tmux capture-pane -e -p -S '-' -J)
 # \u00A0 is a non-breaking space. I use it to determine where my prompt starts and ends so I can view the
 # output of the last command. (see: tmux.fish for how it gets added to my prompt).
