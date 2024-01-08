4f00fcb32d9a7763c2305cd69cd9a2ad471e14f1
Biggie Emmanuel <code@bigo.lu>
2024-01-07 23:08:38 -0500

    (home-manager) remove nix-direnv
    The project that uses it should be including it

--- flake-modules/home-manager/modules/direnv.nix
+++ flake-modules/home-manager/modules/direnv.nix
@@ -2,7 +2,6 @@
   {
     home.packages = with pkgs; [
       direnv
-      nix-direnv
     ];
 
     repository.symlink.xdg.configFile = {
@@ -10,12 +9,6 @@
     };
 
     xdg.configFile = {
-      # I'm using an out-of-store symlink because since links are generated before programs are installed, if
-      # nix-direnv hasn't been installed yet, a regular symlink would throw an error. For out-of-store links
-      # however, Home Manager doesn't require that the source file exists before making the link.
-      "direnv/lib/use_nix_direnv.sh".source =
-        config.lib.file.mkOutOfStoreSymlink "${config.home.profileDirectory}/share/nix-direnv/direnvrc";
-
       "fish/conf.d/direnv.fish".source = ''${
         pkgs.runCommand "direnv-config.fish" {} "${pkgs.direnv}/bin/direnv hook fish > $out"
       }'';
