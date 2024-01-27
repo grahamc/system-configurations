924c7d684ef81693da9315a946e0d0c9dcbf85d0
Biggie Emmanuel <code@bigo.lu>
2024-01-27 16:52:12 -0500

    (neovim) set fold text to empty string to preserve syntax highlighting

--- dotfiles/neovim/lua/terminal/folds.lua
+++ dotfiles/neovim/lua/terminal/folds.lua
@@ -1,5 +1,6 @@
 vim.o.foldlevelstart = 99
 vim.opt.fillchars:append("fold: ")
+vim.o.foldtext = ""
 vim.keymap.set("n", "<Tab>", function()
   vim.cmd([[silent! normal! za]])
 end)
@@ -43,29 +44,6 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = SetDefaultFoldMethod,
   group = vim.api.nvim_create_augroup("SetDefaultFoldMethod", {}),
 })
-
-function FoldText()
-  local window_width = vim.fn.winwidth(0)
-  local gutter_width = vim.fn.getwininfo(vim.fn.win_getid())[1].textoff
-  local line_width = window_width - gutter_width
-
-  local fold_line_count = (vim.v.foldend - vim.v.foldstart) + 1
-  local fold_description = string.format("(%s)", fold_line_count)
-  local fold_description_length = vim.fn.strdisplaywidth(fold_description)
-
-  local separator_text = "⋯ "
-  local separator_text_length = 2
-
-  local line_text = vim.fn.getline(vim.v.foldstart)
-  -- truncate if there isn't space for the fold description and separator text
-  local max_line_text_length = line_width - (fold_description_length + separator_text_length)
-  if vim.fn.strdisplaywidth(line_text) > max_line_text_length then
-    line_text = string.sub(line_text, 1, max_line_text_length)
-  end
-
-  return line_text .. separator_text .. fold_description
-end
-vim.o.foldtext = "v:lua.FoldText()"
 
 -- Use folds provided by a language server
 Plug("pierreglaser/folding-nvim")
