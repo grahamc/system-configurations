daca6368e0d6731e60a5c799d4b6129b74dd54c0
Biggie Emmanuel <code@bigo.lu>
2024-02-26 10:13:22 -0500

    (bat) underline the highlighted line
    the theme needs to be named ansi to do that, details in theme file

--- flake-modules/home-manager/modules/bat.nix
+++ flake-modules/home-manager/modules/bat.nix
@@ -5,7 +5,7 @@
 }: {
   repository.symlink.xdg.configFile = {
     "bat/config".source = "bat/config";
-    "bat/themes/base256-brighter.tmTheme".source = "bat/base256-brighter.tmTheme";
+    "bat/themes/ansi.tmTheme".source = "bat/ansi.tmTheme";
   };
 
   home.packages = with pkgs; [
