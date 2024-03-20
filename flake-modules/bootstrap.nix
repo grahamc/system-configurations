8da55b63ea926932228c69f07b432bc369c113c0
Biggie Emmanuel <code@bigo.lu>
2024-03-20 10:26:23 -0400

    don't make firstaide releases

--- flake-modules/bootstrap.nix
+++ flake-modules/bootstrap.nix
@@ -9,30 +9,6 @@
   }: let
     inherit (lib.attrsets) optionalAttrs;
     inherit (pkgs.stdenv) isDarwin;
-
-    # TODO: would be nice if firstaide started doing releases again
-    firstaidePackage = pkgs.rustPlatform.buildRustPackage {
-      pname = "firstaide";
-      version = "master";
-      src = inputs.firstaide;
-
-      cargoLock = {
-        lockFile = "${inputs.firstaide}/Cargo.lock";
-      };
-
-      # for Linux
-      nativeBuildInputs = [pkgs.pkg-config];
-      buildInputs = [pkgs.openssl];
-      # TODO: These probably aren't necessary, but I'm too lazy to test
-      PKG_CONFIG_PATH = "${pkgs.openssl.dev.outPath}/lib/pkgconfig:";
-      PATH = "${pkgs.pkg-config}/bin";
-
-      meta = {
-        description = "Bootstrap tool for Nix environments";
-        homepage = "https://github.com/NoRedInk/firstaide";
-        mainProgram = "firstaide";
-      };
-    };
     supportedSystems = with inputs.flake-utils.lib.system; [x86_64-linux x86_64-darwin];
     isSupportedSystem = builtins.elem system supportedSystems;
     outputs = optionalAttrs isSupportedSystem {
@@ -40,7 +16,6 @@
         {
           inherit (pkgs) nix;
           homeManager = inputs'.home-manager.packages.default;
-          firstaide = firstaidePackage;
         }
         // optionalAttrs isDarwin {
           nixDarwin = inputs'.nix-darwin.packages.default;
